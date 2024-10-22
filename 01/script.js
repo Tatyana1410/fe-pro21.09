55423652e4bc4d23c59cfbc2bb146bf43fd57746
Tatyana1410 <tatyana.istowka@gmail.com>
2024-10-22 15:47:13 +0200

    HomeWork-01

---
+++ 01/script.js
@@ -0,0 +1,21 @@
+
+let userNickname=`Tatyana`;
+let userScore=8;
+let age=34;
+let number=5;
+let result=age+number;
+console.log(`+ operation ${result}`);
+console.log(`+ operation ${age+5}`);
+console.log(`+operation result ${age}+5 = ${age+number}`);
+
+let isStudent=true;
+console.log(isStudent);
+isStudent=false;
+console.log(isStudent);
+
+let item=10;
+console.log(item);
+item=`ten`
+console.log(item);
+item=true;
+console.log(item);
