2e412dd7e5584f45b4515bca818d6cd4a8334c50
Tatyana1410 <tatyana.istowka@gmail.com>
2024-11-02 14:41:38 +0100

    home-work08

---
+++ 08/script.js
@@ -0,0 +1,33 @@
+// 01
+for( let i=0; i<5; i++){
+    console.log(i);
+};
+
+let message = `test`;
+function example(){
+    if(true){
+        let message = `hello, world`;
+        console.log(message);
+    }
+    console.log(message);
+};
+
+// 02
+const person = {
+    name: 'John',
+    age: 25,
+    occupation: 'Developer'
+};
+for(const key in person){
+    console.log(person[key]);
+};
+
+// 03
+const students = [
+    { name: 'Alice', age: 20, grade: 'A' },
+    { name: 'Bob', age: 22, grade: 'B' },
+    { name: 'Charlie', age: 21, grade: 'C' }
+];
+for(const value of students) {
+    console.log (value.name, value.age, value.grade)
+};
