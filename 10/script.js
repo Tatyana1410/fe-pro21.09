920bc127748a9560f92974f8bf9d81a05c922815
Tatyana1410 <tatyana.istowka@gmail.com>
2024-11-06 16:07:42 +0100

    Home-work10

---
+++ 10/script.js
@@ -0,0 +1,70 @@
+let userValue = prompt (`write your variant`, `rock, paper, scissors`);
+userValue.toLowerCase();
+let arrVariants = [`rock`, `paper`, 'scissors'];
+let compValue = arrVariants[Math.floor (Math.random(arrVariants)*arrVariants.length)];
+console.log(compValue)
+if (userValue === compValue){
+    alert(`dead heat, try again`)}
+    else if (userValue==`rock`&& compValue==`scissors`){
+        alert( `You win`);
+    }
+    else if (userValue==`paper`&& compValue==`rock`){
+        alert( `You win`);
+    }
+    else if (userValue==`scissors`&& compValue==`paper`){
+        alert( `You win`);
+    }
+    else{
+        alert(`You lost. Try again!`);
+    };
+
+// 02
+function mathMin(x,y){
+    a = Math.min(x, y);
+};
+mathMin(5,7);
+
+function mathMax(x,y){
+    b = Math.max(x, y);
+};
+mathMax(5,7);
+
+// 03
+function mathPow(x,y){
+    console.log(Math.pow(x,y));
+};
+mathPow(5,2);
+
+// 04
+function mathFloor(x){
+    c = Math.floor(x);
+};
+mathFloor(5.6)
+
+function mathCeil(x){
+    d = Math.ceil(x);
+};
+mathCeil(5.6);
+
+// 05
+let today = new Date();
+function yearDate(x){
+    console.log(today.getFullYear()-x);
+};
+yearDate(34);
+
+// 06
+let todayDate = new  Date();
+console.log(todayDate.toLocaleString());
+
+// 07
+let newString = `table bad sofa`;
+let ArrnewString = newString.split(` `);
+
+// let result = ArrnewString.map(word=>word.toUpperCase());
+// console.log(result)
+
+console.log(ArrnewString.map(word=>word.toUpperCase()));
+
+
+
