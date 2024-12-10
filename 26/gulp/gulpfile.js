84602437df95ac6392e32c32397765172af62cee
Tatyana1410 <tatyana.istowka@gmail.com>
2024-12-10 19:00:34 +0100

    home-work

--- 26/gulp/gulpfile.js
+++ 26/gulp/gulpfile.js
@@ -1,7 +1,15 @@
 function test(cb) {
-    // place code for your default task here
     console.log(`Hello test`);
     cb();
   }
+  const gulp = require('gulp');
+  const cleanCSS = require('gulp-clean-css');
+  function styles(){
+    return gulp.src(`styles/*.css`)
+    .pipe(cleanCSS({compatibility: `ie8`}))
+    .pipe(gulp.dest(`dist`))
+  }
   
-  exports.default = test
+  exports.default = test;
+  exports.styles=styles;
+
